6d04e30292b24704fddebbd74bebf39a11e26156
Kresimir Kumericki <kkumer@gmail.com>
2023-09-22 11:32:14 +0200

    Initial commit of notebooks

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # nloimpact23
-Code for NLO DVMP impact paper
+
+This is the code for the analysis done for the paper
+
+   * M. Čuić, G. Duplančić, K. Kumerički, and K. Passek-K.,  _NLO corrections to the deeply virtual meson production revisited: impact on the extraction of generalized parton distributions_, to be finished soon
+
+Jupyter/Python notebooks ``nloimpact.ipynb`` and  ``LT_separation.ipynb`` show in detail how numerical results
+and plots in the paper are obtained. They provide also some additional numerics and plots.
+These files are commented and should be readable immediately here on
+the github. If you want to run them yourself, you need the [Gepard](https://gepard.phy.hr) 
+Python package.
+
+If you find bugs, please, report them either through github "Issues" or directly to
+authors via email (easily found at [arXiv](http://arXiv.org)).
---
+++ graphs.py
@@ -0,0 +1,133 @@
+"""Matplotlib graphing setup stolen from BUQEYE people."""
+
+# MIT License
+# 
+# Copyright (c) 2021 BUQEYE (Bayesian Uncertainty Quantification: Errors for Your EFT)
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+import matplotlib as mpl
+
+
+def setup_rc_params(presentation=False, constrained_layout=True, publish=False):
+    if presentation:
+        fontsize = 11
+    else:
+        fontsize = 9
+    black = "k"
+
+    mpl.rcdefaults()  # Set to defaults
+    if publish:
+        mpl.rcParams["text.usetex"] = True
+        # mpl.rcParams["text.latex.preview"] = True
+        mpl.rcParams["font.family"] = "serif"
+
+    # mpl.rc("text", usetex=True)
+    mpl.rcParams["font.size"] = fontsize
+
+    mpl.rcParams["axes.labelsize"] = fontsize
+    mpl.rcParams["axes.edgecolor"] = black
+    # mpl.rcParams['axes.xmargin'] = 0
+    mpl.rcParams["axes.labelcolor"] = black
+    mpl.rcParams["axes.titlesize"] = fontsize
+
+    mpl.rcParams["ytick.direction"] = "in"
+    mpl.rcParams["xtick.direction"] = "in"
+    mpl.rcParams["xtick.labelsize"] = fontsize
+    mpl.rcParams["ytick.labelsize"] = fontsize
+    mpl.rcParams["xtick.color"] = black
+    mpl.rcParams["ytick.color"] = black
+    # Make the ticks thin enough to not be visible at the limits of the plot (over the axes border)
+    mpl.rcParams["xtick.major.width"] = mpl.rcParams["axes.linewidth"] * 0.95
+    mpl.rcParams["ytick.major.width"] = mpl.rcParams["axes.linewidth"] * 0.95
+    # The minor ticks are little too small, make them both bigger.
+    mpl.rcParams["xtick.minor.size"] = 2.4  # Default 2.0
+    mpl.rcParams["ytick.minor.size"] = 2.4
+    mpl.rcParams["xtick.major.size"] = 3.9  # Default 3.5
+    mpl.rcParams["ytick.major.size"] = 3.9
+
+    ppi = 72  # points per inch
+    # dpi = 150
+    mpl.rcParams["figure.titlesize"] = fontsize
+    mpl.rcParams["figure.dpi"] = 150  # To show up reasonably in notebooks
+    mpl.rcParams["figure.constrained_layout.use"] = constrained_layout
+    # 0.02 and 3 points are the defaults:
+    # can be changed on a plot-by-plot basis using fig.set_constrained_layout_pads()
+    mpl.rcParams["figure.constrained_layout.wspace"] = 0.0
+    mpl.rcParams["figure.constrained_layout.hspace"] = 0.0
+    mpl.rcParams["figure.constrained_layout.h_pad"] = 3.0 / ppi  # 3 points
+    mpl.rcParams["figure.constrained_layout.w_pad"] = 3.0 / ppi
+
+    mpl.rcParams["legend.title_fontsize"] = fontsize
+    mpl.rcParams["legend.fontsize"] = fontsize
+    mpl.rcParams[
+        "legend.edgecolor"
+    ] = "inherit"  # inherits from axes.edgecolor, to match
+    mpl.rcParams["legend.facecolor"] = (
+        1,
+        1,
+        1,
+        0.6,
+    )  # Set facecolor with its own alpha, so edgecolor is unaffected
+    mpl.rcParams["legend.fancybox"] = True
+    mpl.rcParams["legend.borderaxespad"] = 0.8
+    mpl.rcParams[
+        "legend.framealpha"
+    ] = None  # Do not set overall alpha (affects edgecolor). Handled by facecolor above
+    mpl.rcParams[
+        "patch.linewidth"
+    ] = 0.8  # This is for legend edgewidth, since it does not have its own option
+
+    mpl.rcParams["hatch.linewidth"] = 0.5
+
+    # added by kkumer:
+    mpl.rcParams['legend.frameon'] = False
+    mpl.rcParams['text.latex.preamble'] = r'\usepackage{amsmath} \usepackage{amssymb}'
+
+    # bbox = 'tight' can distort the figure size when saved (that's its purpose).
+    # mpl.rc('savefig', transparent=False, bbox='tight', pad_inches=0.04, dpi=350, format='png')
+    mpl.rc("savefig", transparent=False, bbox=None, dpi=400, format="pdf")
+
+
+def lighten_color(color, amount=0.5):
+    """
+    Lightens the given color by multiplying (1-luminosity) by the given amount.
+    Input can be matplotlib color string, hex string, or RGB tuple.
+
+    Examples:
+    >> lighten_color('g', 0.3)
+    >> lighten_color('#F034A3', 0.6)
+    >> lighten_color((.3,.55,.1), 0.5)
+    """
+    import colorsys
+
+    import matplotlib.colors as mc
+
+    try:
+        c = mc.cnames[color]
+    except:
+        c = color
+    c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+    return colorsys.hls_to_rgb(c[0], 1 - amount * (1 - c[1]), c[2])
+
+
+def darken_color(color, amount=0.5):
+    """
+    Darken the given color by multiplying (1-luminosity) by the given amount.
+    Input can be matplotlib color string, hex string, or RGB tuple.
+
+    Examples:
+    >> darken_color('g', 0.3)
+    >> darken_color('#F034A3', 0.6)
+    >> darken_color((.3,.55,.1), 0.5)
+    """
+    return lighten_color(color, 1.0 / amount)
+
